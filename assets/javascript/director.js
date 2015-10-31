81efde887c8edbebfb55aa8d6baca5ae641fe36b
Geoffrey Goh <kg422@cornell.edu>
2015-10-31 16:51:15 -0700

    update images

--- assets/javascript/director.js
+++ assets/javascript/director.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-	$('#gallery').backstretch([
-		"/assets/images/toh2.jpg",
-		"/assets/images/toh.jpg"
-	], {duration: 3000, fade: 750});
+	$('#gallery').backstretch(shuffle([
+		"/assets/images/tohgallery/toh.jpg",
+		"/assets/images/tohgallery/toh2.jpg",
+		"/assets/images/tohgallery/toh3.jpg",
+		"/assets/images/tohgallery/toh4.jpg",
+		"/assets/images/tohgallery/toh5.jpg"
+	]), {duration: 3000, fade: 750});
 });
--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-	$('#gallery').backstretch([
-		"/assets/images/festival2.jpg",
-		"/assets/images/prix.jpg",
-		"/assets/images/sota.jpg",
-		"/assets/images/festival.jpg"
-	], {duration: 3000, fade: 750});
+	$('#gallery').backstretch(shuffle([
+		"/assets/images/singersgallery/singers.jpg",
+		"/assets/images/singersgallery/singers2.jpg",
+		"/assets/images/singersgallery/singers3.jpg",
+		"/assets/images/singersgallery/singers4.jpg",
+		"/assets/images/singersgallery/singers5.jpg",
+		"/assets/images/singersgallery/singers6.jpg",
+		"/assets/images/singersgallery/singers7.jpg",
+		"/assets/images/singersgallery/singers8.jpg",
+		"/assets/images/singersgallery/singers9.jpg",
+		"/assets/images/singersgallery/singers10.jpg"
+	]), {duration: 3000, fade: 750});
 	$("table").addClass("table");
 	$("table").addClass("table-hover");
 	$("th:first-child").css("width", "30%");
